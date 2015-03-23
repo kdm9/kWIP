712f74a0cf300816ff8a2d10bf146bda8abe4c52
Kevin Murray <spam@kdmurray.id.au>
2015-03-24 10:34:31 +1100

    Fix license header of main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,19 @@
-//
-// This file is part of khmer, http://github.com/ged-lab/khmer/, and is
-// Copyright (C) Michigan State University, 2009-2013. It is licensed under
-// the three-clause BSD license; see doc/LICENSE.txt.
-// Contact: khmer-project@idyll.org
-//
+/*
+ * Copyright 2015 Kevin Murray <spam@kdmurray.id.au>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "khmer.hh"
 #include "counting.hh"
