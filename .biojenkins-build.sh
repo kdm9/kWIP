b26d13e15070ba001217c6235da88b8cb8f8791e
Kevin Murray <spam@kdmurray.id.au>
2015-04-08 15:24:57 +1000

    Use absolute path in biojenkins config

--- .biojenkins-build.sh
+++ .biojenkins-build.sh
@@ -1,19 +1,21 @@
 
+wdir=`pwd`
+
 test -d khmer-src || git clone https://github.com/ged-lab/khmer khmer-src
 
 # update and install khmer
 pushd khmer-src
 	git fetch --all
 	git reset --hard origin/master
-	rm -rf ../khmer-target
-	mkdir ../khmer-target
+	rm -rf $wdir/khmer-target
+	mkdir $wdir/khmer-target
 	pushd lib
-		make install PREFIX=../../khmer-target/
+		make install PREFIX=$wdir/khmer-target/
 	popd
 popd
 
 
 mkdir build && cd build
 
-cmake .. -DKHMER_ROOT=../khmer-target/
+cmake .. -DKHMER_ROOT=$wdir/khmer-target/
 make
