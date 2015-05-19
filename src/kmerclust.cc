a2676a46344c7c81cc183469c08cd1ff59f8d070
Kevin Murray <spam@kdmurray.id.au>
2015-05-19 15:24:19 +1000

    Add kmerclust source for util code

---
+++ src/kmerclust.cc
@@ -0,0 +1,21 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  kmerclust.cc
+ *    Description:  Utilities for kmerclust
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#include <string>
+
+#include "kmerclust-config.hh"
+
+namespace kmerclust
+{
+
+const std::string kmerclust_version = KMERCLUST_VERSION;
+
+} // end namespace kmerclust
