90556cdc286c64a7471a383455491462f67199e1
Kevin Murray <spam@kdmurray.id.au>
2015-05-21 20:45:52 +1000

    Fix biojenkins build process
    - Use push/popd
    - Use full path to readme in install instruction testing

--- .biojenkins-build.sh
+++ .biojenkins-build.sh
@@ -7,22 +7,24 @@ test -d khmer-src || git clone https://github.com/ged-lab/khmer khmer-src
 pushd khmer-src
 	git fetch --all
 	git reset --hard origin/master
-	rm -rf $wdir/khmer-target
-	mkdir $wdir/khmer-target
+	rm -rf $wdir/khmer
+	mkdir $wdir/khmer
 	pushd lib
-		make install PREFIX=$wdir/khmer-target/
+		make install PREFIX=$wdir/khmer/
 	popd
 popd
 
 
+# Build kmerclust
 rm -rf build
-mkdir build && cd build
-
-cmake $wdir -DKHMER_ROOT=$wdir/khmer-target/
-make
+mkdir build
+pushd build
+	cmake $wdir -DKHMER_ROOT=$wdir/khmer/
+	make
+popd
 
 # check that we can follow the instructions provided for installation
 mkdir $wdir/not_home
 # Process: extract commands, change home directory to above, run
-grep '^    ' README.md |sed 's,$HOME,$wdir/not_home,g' | bash -xe
+grep '^    ' $wdir/README.md |sed 's,$HOME,$wdir/not_home,g' | bash -xe
 rm -rf $wdir/not_home
