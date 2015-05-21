307b340684530370fd309ff2d5367be5c772d36c
Kevin Murray <spam@kdmurray.id.au>
2015-05-21 22:35:17 +1000

    Reformat & fix installation test in biojenkins.sh

--- .biojenkins-build.sh
+++ .biojenkins-build.sh
@@ -7,13 +7,13 @@ test -d khmer-src || git clone https://github.com/ged-lab/khmer khmer-src
 
 # update and install khmer
 pushd khmer-src
-	git fetch --all
-	git reset --hard origin/master
-	rm -rf $wdir/khmer
-	mkdir $wdir/khmer
-	pushd lib
-		make install PREFIX=$wdir/khmer/
-	popd
+    git fetch --all
+    git reset --hard origin/master
+    rm -rf $wdir/khmer
+    mkdir $wdir/khmer
+    pushd lib
+        make install PREFIX=$wdir/khmer/
+    popd
 popd
 
 
@@ -21,12 +21,14 @@ popd
 rm -rf build
 mkdir build
 pushd build
-	cmake $wdir -DKHMER_ROOT=$wdir/khmer/
-	make
+    cmake $wdir -DKHMER_ROOT=$wdir/khmer/
+    make
 popd
 
 # check that we can follow the instructions provided for installation
-rm -rf $wdir/not_home
-mkdir $wdir/not_home
-# Process: extract commands, change home directory to above, run
-grep '^    ' $wdir/README.md |sed 's,$HOME,$wdir/not_home,g' | bash -xe
+rm -rf $wdir/install_test
+mkdir $wdir/install_test
+pushd $wdir/install_test
+    # Process: extract commands, change home directory to above, run
+    grep '^    ' $wdir/README.md | sed 's,$HOME,$wdir/install_test,g' | bash -xe
+popd
