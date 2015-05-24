e277a415b4ea421469a391ce838f2c30759b99de
Kevin Murray <spam@kdmurray.id.au>
2015-05-24 14:06:12 +1000

    Rewrite img.R, includes kernel matrix, sorts mats

--- util/img.R
+++ util/img.R
@@ -2,20 +2,34 @@
 library(RColorBrewer)
 argv = commandArgs(trailingOnly=T)
 dist.file = paste0(argv[1], ".dist")
+kern.file = paste0(argv[1], ".kern")
 
 base = basename(argv[1])
 dist = as.matrix(read.delim(dist.file)[, -1])
+kern = as.matrix(read.delim(kern.file)[, -1])
 n = ncol(dist)
 
+clust = hclust(as.dist(dist))
+sam_ord = clust$order
+
 pdf(paste0(base, ".pdf"))
-image(1:n, 1:n, dist,
+image(1:n, 1:n, dist[sam_ord, sam_ord],
       axes=FALSE,
       xlab="",
       ylab="",
       col=brewer.pal(9, "Blues"),
       main=paste("Distance of", base))
-axis(1, 1:n, colnames(dist), srt=45, xpd=T, tick=F)
-axis(2, 1:n, colnames(dist), srt=90, tick=F)
+axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
+axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
 
-plot(hclust(as.dist(dist)), main=base)
+image(1:n, 1:n, kern[sam_ord, sam_ord],
+      axes=FALSE,
+      xlab="",
+      ylab="",
+      col=brewer.pal(9, "Blues"),
+      main=paste("Kernel of", base))
+axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
+axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
+
+plot(clust, cex=0.2, main=base)
 dev.off()
