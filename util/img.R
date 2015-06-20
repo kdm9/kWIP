bc1545960a49bcb8f2d251d14a5440a2b61449bf
Kevin Murray <spam@kdmurray.id.au>
2015-06-20 12:28:53 +1000

    Update img.R to use image.plot for legend bar

--- util/img.R
+++ util/img.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-library(RColorBrewer)
+library(fields)
 argv = commandArgs(trailingOnly=T)
 dist.file = paste0(argv[1], ".dist")
 kern.file = paste0(argv[1], ".kern")
@@ -11,23 +11,24 @@ n = ncol(dist)
 
 clust = hclust(as.dist(dist))
 sam_ord = clust$order
+sam_ord = 1:n
 
 pdf(paste0(base, ".pdf"))
-image(1:n, 1:n, dist[sam_ord, sam_ord],
-      axes=FALSE,
-      xlab="",
-      ylab="",
-      col=brewer.pal(9, "Blues"),
-      main=paste("Distance of", base))
+image.plot(1:n, 1:n, dist[sam_ord, sam_ord],
+           axes=FALSE,
+           xlab="",
+           ylab="",
+           col=rainbow(80, start=0.2),
+           main=paste("Distance of", base))
 axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
 axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
 
-image(1:n, 1:n, kern[sam_ord, sam_ord],
-      axes=FALSE,
-      xlab="",
-      ylab="",
-      col=brewer.pal(9, "Blues"),
-      main=paste("Kernel of", base))
+image.plot(1:n, 1:n, kern[sam_ord, sam_ord],
+           axes=FALSE,
+           xlab="",
+           ylab="",
+           col=rainbow(80, start=0.2),
+           main=paste("Kernel of", base))
 axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
 axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
 
