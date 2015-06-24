3dacd21c333cff9432948342b57168e460ad3b38
Kevin Murray <spam@kdmurray.id.au>
2015-06-24 15:04:10 +1000

    Add norm kernel and MDS to image.R script

--- util/img.R
+++ util/img.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-library(fields)
+library(fields, quietly = T)
 argv = commandArgs(trailingOnly=T)
 dist.file = paste0(argv[1], ".dist")
 kern.file = paste0(argv[1], ".kern")
@@ -11,7 +11,6 @@ n = ncol(dist)
 
 clust = hclust(as.dist(dist))
 sam_ord = clust$order
-sam_ord = 1:n
 
 pdf(paste0(base, ".pdf"))
 image.plot(1:n, 1:n, dist[sam_ord, sam_ord],
@@ -32,5 +31,28 @@ image.plot(1:n, 1:n, kern[sam_ord, sam_ord],
 axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
 axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
 
+d = diag(kern)
+geom.mean.d = sqrt(d %*% t(d))
+normk = kern / geom.mean.d
+
+image.plot(1:n, 1:n, normk[sam_ord, sam_ord],
+           axes=FALSE,
+           xlab="",
+           ylab="",
+           col=rainbow(80, start=0.2),
+           main=paste("Normalised Kernel of", base))
+axis(1, 1:n, colnames(dist)[sam_ord], srt=45, xpd=T, cex.axis=0.2, las=2, tick=F)
+axis(2, 1:n, colnames(dist)[sam_ord], srt=90, cex.axis=0.2, las=2, tick=F)
+
 plot(clust, cex=0.2, main=base)
+
+fit <- cmdscale(dist, eig=TRUE, k=2) # k is the number of dim
+
+# plot solution
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",
+  main="Metric MDS", type='p')
+#text(x, y, labels = colnames(dist), cex=.4)
+
 dev.off()
