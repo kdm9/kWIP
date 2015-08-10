3190bff878f8b6106deb603a5d9556e86e1bd9e7
Kevin Murray <spam@kdmurray.id.au>
2015-08-10 19:40:30 +1000

    Actual run make/make test in simple install script

--- util/simple-install.sh
+++ util/simple-install.sh
@@ -2,9 +2,11 @@
 
 rm -rf build
 mkdir -p build
-
 cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$HOME -DCMAKE_BUILD_TYPE=Release ..
+make
+make test
 
-echo OK, now cd into './build' and type make and/or make install
+echo Everything worked!
+echo Please cd into './build' and type make install
