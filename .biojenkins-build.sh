6c2491a5e1d39a0fb94648e5387db93cfeae96b8
Kevin Murray <spam@kdmurray.id.au>
2015-11-06 19:49:34 +1100

    Fix/update CI build script for bundled khmer

--- .biojenkins-build.sh
+++ .biojenkins-build.sh
@@ -4,28 +4,6 @@ export wdir=`pwd`
 
 BUILD_TYPE="${BUILD_TYPE:-Debug}"
 
-if [ "$FORCE_KHMER_DOWNLOAD" -ne 0 ]
-then
-    rm -rf $wdir/khmer
-    unset DOXLIROOT_ARG
-else
-    # Ensure we have khmer's source
-    test -d khmer-src || git clone https://github.com/ged-lab/khmer khmer-src
-
-    # update and install khmer
-    pushd khmer-src
-        git fetch --all
-        git reset --hard v2.0
-        rm -rf $wdir/khmer
-        mkdir $wdir/khmer
-        pushd lib
-            make clean all install PREFIX=$wdir/khmer/
-        popd
-    popd
-
-    DOXLIROOT_ARG="-DOXLI_ROOT=$wdir/khmer/"
-fi
-
 # Build kwip
 rm -rf build target
 mkdir build
@@ -34,7 +12,6 @@ pushd build
     cmake \
         $wdir \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-        $DOXLIROOT_ARG \
         -DCMAKE_INSTALL_PREFIX=$wdir/target
     make
     ctest --verbose
