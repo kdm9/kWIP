1a7c67f43202b8d29b1820fbedad4d7128f51109
Kevin Murray <spam@kdmurray.id.au>
2016-03-18 12:24:43 +1100

    Refactor core functions out of main scripts
    Also rename CLI programs for consistency

---
+++ kwipy/kwipy/_logging.py
@@ -0,0 +1,42 @@
+# Copyright 2016 Kevin Murray <spam@kdmurray.id.au>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function, division
+
+from blessings import Terminal
+
+from sys import stdout, stderr
+
+
+term = Terminal()
+
+
+def progress(*args, file=stdout, end=''):
+    file.write(term.move_x(0))
+    file.write(term.clear_eol())
+    print(*args, end=end, file=file)
+    file.flush()
+
+
+def info(*args, file=stdout, end='\n'):
+    end += term.normal
+    print(term.blue, *args, file=file, end=end)
+    file.flush()
+
+
+def warn(*args, file=stderr, end='\n'):
+    end += term.normal
+    print(term.bold_yellow, *args, file=file, end=end)
+    file.flush()
