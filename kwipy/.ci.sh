66f9e258dfaca3e75c2b59c89f1e5df45defa2a7
Kevin Murray <spam@kdmurray.id.au>
2016-04-09 22:11:38 +1000

    travis: kwipy build should use conda

--- kwipy/.ci.sh
+++ kwipy/.ci.sh
@@ -1,6 +1,14 @@
 
 set -xe
 
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy cython numexpr
+source activate test-environment
+
 python3 setup.py install
 python3 setup.py test
 
