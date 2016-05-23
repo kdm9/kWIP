7b437d87f23488ed0dda3ec193bf98ee37602515
Kevin Murray <spam@kdmurray.id.au>
2016-05-23 13:23:16 +1000

    Numeric bug fixes in kernel & weight calc

--- kwipy/kwipy/internals.pyx
+++ kwipy/kwipy/internals.pyx
@@ -1,7 +1,7 @@
 import numpy as np
 cimport numpy as np
 cimport cython
-from libc.math cimport log2
+from libc.math cimport log2, isnan
 
 ctypedef unsigned short u16
 ctypedef unsigned long long u64
@@ -17,15 +17,20 @@ def popfreq_add_sample(np.ndarray[float, ndim=1] popfreq, np.ndarray[u16, ndim=1
 @cython.boundscheck(False)
 @cython.overflowcheck(False)
 @cython.wraparound(False)
+@cython.cdivision(True)
 def popfreq_to_weights(np.ndarray[float, ndim=1] popfreq, u64 nelem, float nsamples):
     """In-place convertion of population abundance counts to entropy weights."""
+    cdef float freq, freq1m, ent
     for i in range(nelem):
         freq = popfreq[i] / nsamples
         if freq == 0. or freq == 1.:
+            # entropy is zero, but is caclulated as NAN
             popfreq[i] = 0.
             continue
         freq1m = 1 - freq
         ent = -((freq * log2(freq)) + (freq1m) * log2(freq1m))
+        if isnan(ent):
+            print(freq, freq1m, ent)
         popfreq[i] = ent
 
 
@@ -36,6 +41,11 @@ def wipkernel(np.ndarray[u16, ndim=1] A, np.ndarray[u16, ndim=1] B,
               np.ndarray[float, ndim=1] weights, u64 nelem):
     """Weighted inner product kernel"""
     cdef double kernel = 0.0
+    cdef double a, b, w
     for i in range(nelem):
-        kernel += A[i] * B[i] * weights[i]
+        # These are separate here to ensure fpmath is used in kernel calculation
+        a = A[i]
+        b = B[i]
+        w = weights[i]
+        kernel += a * b * w
     return kernel
