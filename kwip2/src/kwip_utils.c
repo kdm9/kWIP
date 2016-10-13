1fd38c20806dcbad84386c82bb9ca770a212551c
Kevin Murray <spam@kdmurray.id.au>
2016-10-13 16:14:56 +1100

    utils: add print version and mkdir -f

--- kwip2/src/kwip_utils.c
+++ kwip2/src/kwip_utils.c
@@ -1,5 +1,8 @@
 #include "kwip_utils.h"
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 #include <math.h>
 
 size_t kwip_parse_size(const char *sizestr)
@@ -18,4 +21,17 @@ size_t kwip_parse_size(const char *sizestr)
     }
 
     return size;
+}
+
+void kwip_print_version(FILE *stream)
+{
+    fprintf(stream, "kWIP version " KWIP_VERSION "\n");
+}
+
+int kwip_mkdirp(const char *dir)
+{
+    int res = mkdir(dir, S_IRWXU | S_IRWXG);
+    if (res == 0) return res;
+    else if (errno == EEXIST) return 0;
+    else return -1;
 }
--- kwip2/src/kwip_utils.h
+++ kwip2/src/kwip_utils.h
@@ -3,9 +3,13 @@
 
 #include "kwip_config.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 size_t kwip_parse_size(const char *sizestr);
 
+void kwip_print_version(FILE *stream);
+
+int kwip_mkdirp(const char *dir);
 
 
 #endif /* end of include guard: KWIP_UTILS_H_GNLDS27X */
