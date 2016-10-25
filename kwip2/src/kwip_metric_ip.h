0b50a18b2c8625bd2fc4f0ee7be25cc77044d31b
Kevin Murray <spam@kdmurray.id.au>
2016-10-25 17:15:17 +1100

    Remove unused headers

--- kwip2/src/kwip_metric_ip.h
+++
@@ -1,5 +0,0 @@
-#ifndef KWIP_METRIC_IP_H_0WHFFPAJ
-#define KWIP_METRIC_IP_H_0WHFFPAJ
-
-
-#endif /* end of include guard: KWIP_METRIC_IP_H_0WHFFPAJ */
--- kwip2/src/kwip_metric_manhattan.h
+++
@@ -1,9 +0,0 @@
-#ifndef KWIP_METRIC_MANHATTAN_H_VZ7GQHUE
-#define KWIP_METRIC_MANHATTAN_H_VZ7GQHUE
-
-#include "kwip_config.h"
-#include "kwip_array.h"
-
-
-#endif /* end of include guard: KWIP_METRIC_MANHATTAN_H_VZ7GQHUE */
-
