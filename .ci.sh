d440aa71fe5a98d63fc4d2453f3d7f076a7e6bcc
Kevin Murray <kdmfoss@gmail.com>
2019-07-27 22:22:12 +0200

    Add travis & ci script

--- .ci.sh
+++ .ci.sh
@@ -1,18 +1,21 @@
-
 set -xe
 
-cd src
-make test
-exit 0
-
-test -f miniconda.sh || wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-test -d miniconda || bash miniconda.sh -b -p ./miniconda
-export PATH="$PWD/miniconda/bin:$PATH"
-conda config --set always_yes true --set changeps1 false
+# setup
+test -d $HOME/miniconda/bin || (wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh && bash miniconda.sh -b -u -p $HOME/miniconda)
+export PATH="$HOME/miniconda/bin:$PATH"
+source $HOME/miniconda/etc/profile.d/conda.sh
+hash -r
+conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda create -q -n test-environment numpy cython pytables
-source activate test-environment
+conda info -a
+conda init
 
+# prepare env
+conda create -q -n test-environment numpy cython pytables
+conda activate test-environment
+
+# build & run tests
 python3 setup.py install
 python3 setup.py test
 
+kwipy-count --help
